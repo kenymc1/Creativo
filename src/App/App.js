88b51451adf590da211687c381b49b27cb2938f7
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-06-30 17:59:10 -0500

    added seed data

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,62 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
+
+// import Auth from '../components/Auth/Auth';
+// import MyNavbar from '../components/MyNavbar/MyNavbar';
+// import BoardContainer from '../components/BoardContainer/BoardContainer';
+// import SingleBoard from '../components/SingleBoard/SingleBoard';
+
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+    singleBoardId: '',
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  setSingleBoard = (boardId) => {
+    this.setState({ singleBoardId: boardId });
+  }
+
   render() {
+    const { authed, singleBoardId } = this.state;
+
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed && singleBoardId.length === 0) {
+        componentToLoad = <BoardContainer setSingleBoard={this.setSingleBoard}/>;
+      } else if (authed && singleBoardId.length > 0) {
+        componentToLoad = <SingleBoard boardId={singleBoardId} setSingleBoard={this.setSingleBoard}/>;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App">
-        <h2>CREATIVO</h2>
-        <button className="btn btn-info"><i className="fas fa-rocket"></i></button>
+        <TheNavbar authed={authed}/>
+        <h1>Creativo</h1>
+        {loadComponent()}
       </div>
     );
   }
