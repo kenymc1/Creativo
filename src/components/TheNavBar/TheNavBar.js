936dcc98b98cebc21050074a77f386c66f0386cd
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-07-09 23:42:51 -0500

    got accordian to work

--- src/components/TheNavBar/TheNavBar.js
+++ src/components/TheNavBar/TheNavBar.js
@@ -24,11 +24,13 @@ class TheNavBar extends React.Component {
     return (
       <div className="theNavBar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+          <a className="navbar-brand" href="https://creativo-ba22e.web.app/">
           {
             authed
               ? <img src="https://i.imgur.com/TUvjMar.png" width="100px"alt="logo" />
               : ''
-          }
+            }
+            </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
